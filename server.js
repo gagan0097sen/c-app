d5c38663cd6f065394b54302fa6c98eca9e142af
Deepak Nagar <deepak@Deepaks-MacBook-Air.local>
2024-01-06 16:54:30 +0530

    fix this code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
-app.use(express.static(__dirname + '/public')) // for image folder
+app.use(express.static(__dirname + '/')) // for image folder
 // routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -21,29 +21,3 @@ io.on('connection', (socket) => {
     })
 })
 
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
-
-// app.use(cors());
-
-// const PORT = process.env.PORT || 3000;
-
-// http.listen(PORT, () => {
-//     console.log(`listening on port ${PORT}`);
-// });
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', (socket) => {
-//     console.log('connected..');
-//     socket.on('message', (msg) => {
-//         socket.broadcast.emit('message', msg);
-//     });
-// });
