49ad845e327218c2f26900e3806d49111426d426
Deepak Nagar <deepak@Deepaks-MacBook-Air.local>
2024-01-06 14:25:18 +0530

    fix code

--- server.js
+++ server.js
@@ -20,3 +20,30 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message', msg)
     })
 })
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+
+// app.use(cors());
+
+// const PORT = process.env.PORT || 3000;
+
+// http.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`);
+// });
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('connected..');
+//     socket.on('message', (msg) => {
+//         socket.broadcast.emit('message', msg);
+//     });
+// });
