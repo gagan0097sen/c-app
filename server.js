6ec98d30abe4f7293762d7e3c8bae366b8f9b9e2
Deepak Nagar <deepak@Deepaks-MacBook-Air.local>
2024-01-06 13:48:51 +0530

    fix code

--- server.js
+++ server.js
@@ -1,49 +1,22 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// app.use(cors());
-// const http = require('http').createServer(app)
-// const PORT = process.env.PORT || 3000;
-// http.listen(PORT, () => {
-//     console.log(`listening on port ${PORT}`)
-// })
-// app.use(express.static(__dirname + '/public')) // for image folder
-// // routes
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-// // socket 
-// const io = require('socket.io')(http)
-// io.on('connection', (socket) => {
-//     console.log('connected..')
-//     socket.on('message', (msg) => {
-//         socket.broadcast.emit('message', msg)
-//     })
-// })
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
 app.use(cors());
-
+const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000;
-
 http.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
-
-app.use(express.static(__dirname + '/public'));
-
+    console.log(`listening on port ${PORT}`)
+})
+app.use(express.static(__dirname + '/public')) // for image folder
+// routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-});
-
+})
+// socket 
+const io = require('socket.io')(http)
 io.on('connection', (socket) => {
-    console.log('connected..');
+    console.log('connected..')
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg);
-    });
-});
+        socket.broadcast.emit('message', msg)
+    })
+})
