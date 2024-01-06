edfb6302b92ffadfec55f915a425b00927f2152e
Deepak Nagar <deepak@Deepaks-MacBook-Air.local>
2024-01-06 13:23:05 +0530

    fix code

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 
 
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// app.use(cors());
+
+// const http = require('http').createServer(app)
+// const PORT = process.env.PORT || 3000;
+// http.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`)
+// })
+
+// app.use(express.static(__dirname + '/public')) // for image folder
+
+// // routes
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// })
+
+// // socket 
+// const io = require('socket.io')(http)
+
+// io.on('connection', (socket) => {
+//     console.log('connected..')
+//     socket.on('message', (msg) => {
+//         socket.broadcast.emit('message', msg)
+//     })
+// })
+
+
+
+
+
 const express = require("express");
 const app = express();
+
+const http = require('http');
+const socketIO = require('socket.io');
+
 const cors = require("cors");
 app.use(cors());
 
-const http = require('http').createServer(app)
+app.createServer(http);
+
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
@@ -27,7 +64,4 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message', msg)
     })
 })
-
-
-
 
