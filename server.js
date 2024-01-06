5a46ba965d03d64241a9b6398c543fba7ff11cf2
Deepak Nagar <deepak@Deepaks-MacBook-Air.local>
2024-01-06 12:08:47 +0530

    fix this

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
-app.use(cors());
+
 
 const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000;
